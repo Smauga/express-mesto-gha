4296bebe48f6c0361a50e2aa25f377a300739b9a
Smauga <91209285+Smauga@users.noreply.github.com>
2022-04-30 19:07:29 +0700

    dev: добавить пункт валидации аватара при регистрации

--- app.js
+++ app.js
@@ -25,10 +25,11 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
   }),
 }), createUser);
 
