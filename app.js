c96862bedd388eb6036d7cfebb3a3ce0266b5d7e
Smauga <91209285+Smauga@users.noreply.github.com>
2022-04-30 18:48:13 +0700

    dev: валидировать добавление пользователя с некорректным email

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
